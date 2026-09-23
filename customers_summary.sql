df638b5fd8888ed0671077ebef2c3d8d615cd4fe
Shubhi Dukariya <sdukariya21@gmail.com>
2026-09-23 13:48:11 +0000

    modified and deleted,one file respectively for each

--- customers_summary.sql
+++ customers_summary.sql
@@ -1,6 +1,14 @@
+with raw_customers as (
+    select 1 as customer_id, 'Alice' as customer_name, 'India' as country, 250 as total_spend
+    union all
+    select 2 as customer_id, 'Bob' as customer_name, 'USA' as country, 120 as total_spend
+    union all
+    select 3 as customer_id, 'Charlie' as customer_name, 'India' as country, 450 as total_spend
+)
+
 select 
     country,
     count(customer_id) as total_customers,
     sum(total_spend) as total_revenue
-from {{ ref('stg_customers') }}
+from raw_customers
 group by country
--- stg_customers.sql
+++
@@ -1,5 +0,0 @@
-select 1 as customer_id, 'Alice' as customer_name, 'India' as country, 250 as total_spend
-union all
-select 2 as customer_id, 'Bob' as customer_name, 'USA' as country, 120 as total_spend
-union all
-select 3 as customer_id, 'Charlie' as customer_name, 'India' as country, 450 as total_spend
